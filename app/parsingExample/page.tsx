73750de48380f45cb647bf81ac07adb050a15f62
Jasper Wong <wlcjasper@gmail.com>
2024-10-01 14:39:26 +0100

    Rename

--- app/parsingExample/page.tsx
+++ app/parsingExample/page.tsx
@@ -1,5 +1,5 @@
 import { createClient } from "@/utils/supabase/server"
-import { extractDatesFromXML } from "../parsingExample"
+import { extractDatesFromXML } from "../extractDatesFromXML"
 
 export default async function Index() {
   const supabase = createClient()
