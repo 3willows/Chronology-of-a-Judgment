42fbe4b8e5235edbd33e253ae506bd2a6896c606
Jasper Wong <wlcjasper@gmail.com>
2024-10-01 14:21:12 +0100

    Clean page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import Hero from "@/components/hero"
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps"
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps"
-import { hasEnvVars } from "@/utils/supabase/check-env-vars"
-
 import { GetChronology } from "@/components/get-chronology"
 
 export default async function Index() {
